9b26a0eaab68ccaefe6e4c4f14e05271ac68ccb0
Derek Tia <dtia05@gmail.com>
2012-08-22 16:58:48 -0700

    fb auth

---
+++ app/assets/javascripts/fb.js
@@ -0,0 +1,17 @@
+function login() {
+FB.login(function(response) {
+ if (response.authResponse) {
+     console.log('Welcome!  Fetching your information.... ');
+     FB.api('/me', function(response) {
+       console.log('Good to see you, ' + response.name + '.');
+     });
+   } else {
+     console.log('User cancelled login or did not fully authorize.');
+   }
+ });
+}
+
+//FB.api('/me', function(response) {
+//  alert('Your name is ' + response.name);
+//});
+
