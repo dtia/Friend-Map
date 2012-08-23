b48b3d4b7ce65a492599891d02a0029f5a85d915
Derek Tia <dtia05@gmail.com>
2012-08-23 01:46:25 -0700

    markers on locations

--- app/assets/javascripts/fb.js
+++ app/assets/javascripts/fb.js
@@ -1,17 +1,56 @@
-function login() {
-FB.login(function(response) {
- if (response.authResponse) {
-     console.log('Welcome!  Fetching your information.... ');
-     FB.api('/me', function(response) {
-       console.log('Good to see you, ' + response.name + '.');
-     });
-   } else {
-     console.log('User cancelled login or did not fully authorize.');
-   }
- });
+function getFriendsList(callback) {
+	FB.api('/me/friends', function(response) {
+		friendList = response.data;
+		callback(friendList);
+	});
 }
 
-//FB.api('/me', function(response) {
-//  alert('Your name is ' + response.name);
-//});
+function getFriendsLocations(friendsList, callback) {
+	var locations = [];
+	var currCount = 0;
+	var numFriends = friendList.length;
+	for (var i=0; i<numFriends; i++) {
+		var friendId = friendList[i].id;
+		FB.api('/' + friendId + '?fields=location', function(response) {
+			locationObject = response.location;
+			if (locationObject) {
+				city = response.location.name;
+				if (city != null && locations.indexOf(city) == -1) {
+					locations.push(city);
+				}
+			}
+			currCount += 1;
+			compileFriendsList(locations, currCount, numFriends, callback);
+		});
+	}
+}
 
+function compileFriendsList(locations, currCount, maxFriends, callback) {
+	if (currCount == maxFriends) {
+		callback(locations);
+	}
+}
+
+function addMarkersForLocations(locations) {
+	console.log('geocoding locations and adding markers...');
+	for(var i=0; i<locations.length; i++) {
+		codeAddress(locations[i]);
+	}
+}
+
+function main() {
+	document.getElementById("showfriendsbutton").style.visibility = 'hidden';
+	document.getElementById("plotting").style.visibility = 'visible';
+	
+	// get friends list
+	// get friends' locations
+	console.log('getting friends locations');
+	var friendsListLocations = getFriendsList(function(friendsList) {
+		getFriendsLocations(friendsList, function(locations) {
+			// geocode locations
+			// plot on map
+			addMarkersForLocations(locations);
+			document.getElementById("showfriendsbutton").style.visibility = 'visible';
+		})
+	});
+}
--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -2,25 +2,31 @@ var geocoder;
 var map;
 function initialize() {
   geocoder = new google.maps.Geocoder();
-  var latlng = new google.maps.LatLng(-34.397, 150.644);
+  var latlng = new google.maps.LatLng(37.7577,-122.4376);
   var mapOptions = {
-    zoom: 8,
+    zoom: 2,
     center: latlng,
     mapTypeId: google.maps.MapTypeId.ROADMAP
   }
   map = new google.maps.Map(document.getElementById("map_canvas"), mapOptions);
 }
 
-function codeAddress() {
-  var address = document.getElementById("address").value;
+function codeAddress(address) {
   geocoder.geocode( { 'address': address}, function(results, status) {
     if (status == google.maps.GeocoderStatus.OK) {
-      map.setCenter(results[0].geometry.location);
+      //map.setCenter(results[0].geometry.location);
       var marker = new google.maps.Marker({
-          map: map,
-          position: results[0].geometry.location
+          	map: map,
+          	position: results[0].geometry.location,
+			title: address
       });
-    } else {
+    }
+	else if (status == google.maps.GeocoderStatus.OVER_QUERY_LIMIT) {
+		setTimeout(function() {
+			codeAddress(address);
+		}, 200);	
+	}
+	else {
       alert("Geocode was not successful for the following reason: " + status);
     }
   });
