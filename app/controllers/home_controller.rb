4780c3d577999854a6dfd148a1936bc74da8939b
Derek Tia <dtia05@gmail.com>
2012-10-07 02:56:10 -0700

    adjust ip in controller

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
   def index
     Settings.reload!
-    Location.set_ip(Settings.request_ip)
+    if Rails.env.development?
+      Location.set_ip(Settings.request_ip)
+    else
+      Location.set_ip(request.ip)
+    end
   end
 end
